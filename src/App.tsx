e51d9b4a42847d2ba22398057af7adfc7350f7ff
salomaosilval <salomaosilval@hotmail.com>
2023-06-21 07:28:13 -0400

    :sparkles: feat: add Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+
 export const App = () => {
   return (
     <>
-      <h1>Portfolio</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import { MainSection } from "./sections/MainSection";
+import { ProjecsSection } from "./sections/ProjectsSection";
+import { ContactSection } from "./sections/ContactSection";
+
+export const Home = () => {
+  return (
+    <>
+      <MainSection />
+      <ProjecsSection />
+      <ContactSection />
+    </>
+  );
+};
